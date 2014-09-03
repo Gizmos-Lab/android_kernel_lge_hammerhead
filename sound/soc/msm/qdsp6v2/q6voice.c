c5f51aabef3f0c7b9072f3035b2c44d19ccadc85
engstk <eng.stk@sapo.pt>
2014-09-04 00:31:19 +0100

    dt2w: Prevent touches during phone calls based on flar2 work
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- sound/soc/msm/qdsp6v2/q6voice.c
+++ sound/soc/msm/qdsp6v2/q6voice.c
@@ -28,6 +28,9 @@
 #include "audio_acdb.h"
 #include "q6voice.h"
 
+#ifdef CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE
+#include <linux/input/doubletap2wake.h>
+#endif
 
 #define TIMEOUT_MS 500
 
@@ -39,6 +42,10 @@
 #define CVP_CAL_SIZE 245760
 /* CVS CAL Size: 49152 = 48 * 1024 */
 #define CVS_CAL_SIZE 49152
+
+#ifdef CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE
+bool in_phone_call = false;
+#endif
 
 enum {
 	VOC_TOKEN_NONE,
@@ -4525,6 +4532,9 @@ int voc_end_voice_call(uint32_t session_id)
 
 		v->voc_state = VOC_RELEASE;
 	}
+	#ifdef CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE
+	in_phone_call = false;
+	#endif
 	mutex_unlock(&v->lock);
 	return ret;
 }
@@ -4731,6 +4741,9 @@ int voc_start_voice_call(uint32_t session_id)
 		ret = -EINVAL;
 		goto fail;
 	}
+	#ifdef CONFIG_TOUCHSCREEN_DOUBLETAP2WAKE
+	in_phone_call = true;
+	#endif
 fail:
 	mutex_unlock(&v->lock);
 	return ret;
